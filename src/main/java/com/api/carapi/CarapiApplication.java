d08d50521332bff3a23a78a7bc5e7d81c73dffd9
felipematheus1337 <lipehbr@gmail.com>
2022-08-31 16:26:17 -0300

    project created

---
+++ src/main/java/com/api/carapi/CarapiApplication.java
@@ -0,0 +1,13 @@
+package com.api.carapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/api/carapi/CarapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.carapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
